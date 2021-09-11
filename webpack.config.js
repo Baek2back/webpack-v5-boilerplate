1a9934d8f3793a4affa451948a8a7fec285776f3
Baek2back <dumbuk12@naver.com>
2021-09-11 10:40:53 +0900

    feat: apply dotenv and loader by mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,23 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Dotenv = require("dotenv-webpack");
 const path = require("path");
-const { NODE_ENV } = process.env;
-
-const isDevelopment = NODE_ENV === "development";
 
 const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
 
-const cssRule = ({ exclude, modules, sourceMap, test }) => ({
+const cssRule = ({ exclude, modules, sourceMap, test, mode }) => ({
   test,
   exclude,
   use: [
-    isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+    mode === "development" ? "style-loader" : MiniCssExtractPlugin.loader,
     {
       loader: "css-loader",
       options: {
-        sourceMap: sourceMap || isDevelopment,
+        sourceMap: sourceMap || mode === "development",
         modules: !!modules,
       },
     },
@@ -33,45 +31,49 @@ const cssRule = ({ exclude, modules, sourceMap, test }) => ({
   ],
 });
 
-module.exports = {
-  mode: isDevelopment ? "development" : "production",
-  entry: "./src/index.ts",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.html$/,
-        use: "html-loader",
-      },
-      cssRule({ test: cssRegex, exclude: cssModuleRegex }),
-      cssRule({ test: cssModuleRegex, modules: true }),
-      cssRule({ test: sassRegex, exclude: sassModuleRegex }),
-      cssRule({ test: sassModuleRegex, modules: true }),
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
+module.exports = (env, argv) => {
+  const { mode } = argv;
+  return {
+    mode: mode === "development" ? "development" : "production",
+    entry: "./src/index.ts",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.html$/,
+          use: "html-loader",
+        },
+        cssRule({ test: cssRegex, exclude: cssModuleRegex }),
+        cssRule({ test: cssModuleRegex, modules: true }),
+        cssRule({ test: sassRegex, exclude: sassModuleRegex }),
+        cssRule({ test: sassModuleRegex, modules: true }),
+      ],
     },
-    compress: true,
-    port: 9000,
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [
-    new MiniCssExtractPlugin(),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "public/index.html",
-    }),
-  ],
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "public"),
+      },
+      compress: true,
+      port: 9000,
+    },
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+      new Dotenv(),
+      new MiniCssExtractPlugin(),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: "public/index.html",
+      }),
+    ],
+  };
 };
