053eadc4e23229d70a338f7a1e8203557ab57d71
Baek2back <dumbuk12@naver.com>
2021-10-11 09:38:57 +0900

    ➕ chore: stylelint config, plugin 추가

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,13 +1,23 @@
 module.exports = {
-  extends: ['stylelint-config-standard'],
-  plugins: ['stylelint-scss', 'stylelint-order', 'stylelint-a11y'],
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recommended-scss",
+    "stylelint-config-rational-order",
+    "stylelint-config-prettier",
+  ],
+  plugins: [
+    "stylelint-scss",
+    "stylelint-order",
+    "stylelint-a11y",
+    "stylelint-prettier",
+  ],
   rules: {
-    'at-rule-no-unknown': null,
-    'scss/at-rule-no-unknown': true,
-    'order/properties-alphabetical-order': true,
-    'no-empty-source': null,
-    'rule-empty-line-before': null,
-    'selector-list-comma-newline-after': null,
-    'no-descending-specificity': null
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true,
+    "no-empty-source": null,
+    "rule-empty-line-before": null,
+    "selector-list-comma-newline-after": null,
+    "no-descending-specificity": null,
+    "prettier/prettier": true,
   },
 };
