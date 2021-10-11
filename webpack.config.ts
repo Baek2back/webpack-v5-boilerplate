b6250261f92b7bff451514e3c864a4b487a07e3e
Baek2back <dumbuk12@naver.com>
2021-10-11 10:36:48 +0900

    🔧 chore: 이미지 및 폰트 파일 assets 모듈 적용

--- webpack.config.ts
+++ webpack.config.ts
@@ -89,6 +89,14 @@ const config: Configuration = {
         test: /\.html$/,
         use: "html-loader",
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
       cssRule({
         test: cssRegex,
         exclude: cssModuleRegex,
